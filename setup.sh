c2e233c929118ec15947a3c5e3f55c59e9b56d2a
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-09-16 12:20:23 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@ wget -q https://github.com/xinxin8816/heroku-aria2c-21vianet/raw/master/rclone.z
 unzip -q rclone.zip
 export PATH=$PWD:$PATH
 chmod 777 /app/rclone
+
+curl -o accounts.zip "${SAURL}"
+unzip -q accounts.zip
+export PATH=$PWD:$PATH
+chmod 777 /app/accounts
